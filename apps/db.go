095016c4e10887f9d098ce2ddde21470be52e589
Dipo Yudhis Rana <dipoyudhisrana@gmail.com>
2024-12-27 13:41:56 +0700

    Boilerplate GOYAM

---
+++ apps/db.go
@@ -0,0 +1,43 @@
+package apps
+
+import (
+	"context"
+	"database/sql"
+)
+
+type DBTX interface {
+	ExecContext(ctx context.Context, query string, args ...interface{}) (result sql.Result, err error)
+	PrepareContext(ctx context.Context, query string) (stmt *sql.Stmt, err error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (rows *sql.Rows, err error)
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) (row *sql.Row)
+}
+
+type Queries struct {
+	db DBTX
+}
+
+func New(db DBTX) *Queries {
+	return &Queries{db: db}
+}
+
+func (q *Queries) WithTx(tx *sql.Tx) *Queries {
+	return &Queries{
+		db: tx,
+	}
+}
+
+func (q *Queries) ExecContext(ctx context.Context, query string, args ...interface{}) (result sql.Result, err error) {
+	return q.db.ExecContext(ctx, query, args...)
+}
+
+func (q *Queries) PrepareContext(ctx context.Context, query string) (stmt *sql.Stmt, err error) {
+	return q.db.PrepareContext(ctx, query)
+}
+
+func (q *Queries) QueryContext(ctx context.Context, query string, args ...interface{}) (rows *sql.Rows, err error) {
+	return q.db.QueryContext(ctx, query, args...)
+}
+
+func (q *Queries) QueryRowContext(ctx context.Context, query string, args ...interface{}) (row *sql.Row) {
+	return q.db.QueryRowContext(ctx, query, args...)
+}
